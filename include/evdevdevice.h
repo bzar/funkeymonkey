86bd99395477a05cc8596511a55740b678fff3a6
Teemu Erkkola <teemu.erkkola@iki.fi>
2015-07-09 17:28:22 +0300

    Sane option parsing and basic error handling, support for relative paths for plugins

---
+++ include/evdevdevice.h
@@ -0,0 +1,70 @@
+#ifndef EVDEV_DEVICE_H
+#define EVDEV_DEVICE_H
+
+#include <linux/input.h>
+#include <string>
+#include <array>
+#include <iostream>
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+class EvdevDevice
+{
+public:
+  explicit EvdevDevice(std::string const& path);
+  ~EvdevDevice();
+  input_event poll();
+  bool ready() const;
+private:
+  int _fd;
+  std::array<input_event, 64> _events;
+  int _numEvents;
+  int _currentEvent;
+};
+
+EvdevDevice::EvdevDevice(std::string const& path) :
+  _fd(0), _events(), _numEvents(0), _currentEvent(0)
+{
+  if(access(path.data(), F_OK ) != -1)
+  {
+    _fd = open(path.data(), O_RDONLY | O_NDELAY);
+  }
+  else
+  {
+    std::cerr << "ERROR: Cannot access '" << path << "'. Does it exist?" << std::endl;
+  }
+}
+EvdevDevice::~EvdevDevice()
+{
+  if(_fd)
+    close(_fd);
+}
+input_event EvdevDevice::poll()
+{
+  if(!_fd)
+    return {0};
+
+  if(_currentEvent >= _numEvents)
+  {
+    fd_set fds;
+    timeval timeout = {1, 0};
+    FD_ZERO(&fds);
+    FD_SET(_fd, &fds);
+    if(select(FD_SETSIZE, &fds, NULL, NULL, &timeout ) <= 0)
+      return {0};
+
+    int numBytes = read(_fd, _events.data(), sizeof(input_event) * _events.size());
+    _numEvents = numBytes / sizeof(input_event);
+    _currentEvent = 0;
+  }
+  return _events[_currentEvent++];
+}
+
+bool EvdevDevice::ready() const
+{
+  return _fd != 0;
+}
+
+#endif
---
+++ include/funkeymonkeymoduleloader.h
@@ -0,0 +1,81 @@
+#ifndef FUNKEYMONKEY_MODULE_LOADER_H
+#define FUNKEYMONKEY_MODULE_LOADER_H
+
+#include <linux/input.h>
+#include <string>
+#include <iostream>
+#include <dlfcn.h>
+
+class FunKeyMonkeyModule
+{
+public:
+  FunKeyMonkeyModule(std::string const& path);
+  ~FunKeyMonkeyModule();
+  bool ready() const;
+  void init();
+  void handle(input_event const& e);
+  void destroy();
+private:
+  void *_lib;
+  void (*_init)();
+  void (*_handle)(input_event const&);
+  void (*_destroy)();
+};
+
+FunKeyMonkeyModule::FunKeyMonkeyModule(std::string const& path) :
+  _lib(nullptr), _init(nullptr), _handle(nullptr), _destroy(nullptr)
+{
+  char* absPath = realpath(path.data(), nullptr);
+  if(absPath)
+  {
+    _lib = dlopen(absPath, RTLD_LAZY);
+    delete absPath;
+  }
+  if (!_lib)
+  {
+    std::cerr << "ERROR: Error opening module " << path << ": " << dlerror() << std::endl;
+  }
+  else
+  {
+    auto load = [this](std::string const& name) {
+      void* value = dlsym(_lib, name.data());
+      auto error = dlerror();
+      if(error)
+      {
+        std::cerr << "ERROR: While loading " << name << ": " << error << std::endl;
+        value = nullptr;
+      }
+      return value;
+    };
+    _init = reinterpret_cast<decltype(_init)>(load("init"));
+    _handle = reinterpret_cast<decltype(_handle)>(load("handle"));
+    _destroy =reinterpret_cast<decltype(_destroy)>(load("destroy"));
+  }
+}
+FunKeyMonkeyModule::~FunKeyMonkeyModule()
+{
+  if(_lib)
+    dlclose(_lib);
+}
+bool FunKeyMonkeyModule::ready() const
+{
+  return _lib != 0;
+}
+void FunKeyMonkeyModule::init()
+{
+  if(_init)
+    (*_init)();
+}
+void FunKeyMonkeyModule::handle(input_event const& e)
+{
+  if(_handle)
+    (*_handle)(e);
+
+}
+void FunKeyMonkeyModule::destroy()
+{
+  if(_destroy)
+    (*_destroy)();
+}
+
+#endif

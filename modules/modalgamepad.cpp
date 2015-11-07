a66d64b0078fde8b701660c655c3926b41d9424d
Teemu Erkkola <teemu.erkkola@iki.fi>
2015-11-07 14:11:37 +0200

    Full config file support

--- modules/modalgamepad.cpp
+++ modules/modalgamepad.cpp
@@ -1,5 +1,6 @@
 #include "funkeymonkeymodule.h"
 #include "uinputdevice.h"
+
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -9,6 +10,7 @@
 #include <fstream>
 #include <algorithm>
 #include <cctype>
+#include <unordered_map>
 
 struct Mouse
 {
@@ -31,31 +33,57 @@ struct Mouse
 struct Settings
 {
   enum NubAxisMode {
+    UNKNOWN_NUB_AXIS_MODE,
     LEFT_JOYSTICK_X, LEFT_JOYSTICK_Y,
     RIGHT_JOYSTICK_X, RIGHT_JOYSTICK_Y,
     MOUSE_X, MOUSE_Y,
     SCROLL_X, SCROLL_Y
   };
   enum NubClickMode {
+    UNKNOWN_NUB_CLICK_MODE,
     NUB_CLICK_LEFT, NUB_CLICK_RIGHT,
     MOUSE_LEFT, MOUSE_RIGHT
   };
 
-  NubAxisMode leftNubModeX = MOUSE_X;
-  NubAxisMode leftNubModeY = MOUSE_Y;
-  NubAxisMode rightNubModeX = SCROLL_X;
-  NubAxisMode rightNubModeY = SCROLL_Y;
-  NubClickMode leftNubClickMode = MOUSE_RIGHT;
-  NubClickMode rightNubClickMode = MOUSE_LEFT;
+  NubAxisMode leftNubModeX = LEFT_JOYSTICK_X;
+  NubAxisMode leftNubModeY = LEFT_JOYSTICK_Y;
+  NubAxisMode rightNubModeX = RIGHT_JOYSTICK_X;
+  NubAxisMode rightNubModeY = RIGHT_JOYSTICK_Y;
+  NubClickMode leftNubClickMode = NUB_CLICK_LEFT;
+  NubClickMode rightNubClickMode = NUB_CLICK_RIGHT;
 
   // May be changed from any thread at any time
   int mouseDeadzone = 100;
   int mouseSensitivity = 15;
   int mouseWheelDeadzone = 500;
+
+  std::string configFile;
+};
+
+using NubAxisModeMap = std::unordered_map<std::string, Settings::NubAxisMode>;
+NubAxisModeMap const NUB_AXIS_MODES = {
+{ "left_joystick_x", Settings::LEFT_JOYSTICK_X },
+{ "left_joystick_y", Settings::LEFT_JOYSTICK_Y },
+{ "right_joystick_x", Settings::RIGHT_JOYSTICK_X },
+{ "right_joystick_y", Settings::RIGHT_JOYSTICK_Y },
+  { "mouse_x", Settings::MOUSE_X },
+  { "mouse_y", Settings::MOUSE_Y },
+  { "scroll_x", Settings::SCROLL_X },
+  { "scroll_y", Settings::SCROLL_Y }
+};
+
+using NubClickModeMap = std::unordered_map<std::string, Settings::NubClickMode>;
+NubClickModeMap const NUB_CLICK_MODES = {
+  { "nub_click_left", Settings::NUB_CLICK_LEFT },
+  { "nub_click_right", Settings::NUB_CLICK_RIGHT },
+  { "mouse_left", Settings::MOUSE_LEFT },
+  { "mouse_right", Settings::MOUSE_RIGHT }
 };
 
 void handleArgs(char const** argv, unsigned int argc, Settings& settings);
 void loadConfig(std::string const& filename, Settings& settings);
+Settings::NubAxisMode parseNubAxisMode(std::string const& str);
+Settings::NubClickMode parseNubClickMode(std::string const& str);
 void handleNubAxis(Settings::NubAxisMode mode, int value, Mouse* mouse, UinputDevice* gamepad, Settings const& settings);
 void handleNubClick(Settings::NubClickMode mode, int value, Mouse* mouse, UinputDevice* gamepad, Settings const& settings);
 
@@ -95,6 +123,11 @@ void init(char const** argv, unsigned int argc)
         global.mouse, &global.settings, &global.stop));
 
   handleArgs(argv, argc, global.settings);
+
+  if(!global.settings.configFile.empty())
+  {
+    loadConfig(global.settings.configFile, global.settings);
+  }
 }
 
 void handle(input_event const& e)
@@ -163,19 +196,64 @@ void handleArgs(char const** argv, unsigned int argc, Settings& settings)
     std::smatch match;
     if(std::regex_match(arg, match, configRegex))
     {
-      configFile = match[1];
+      settings.configFile = match[1];
     }
-  }
-
-  if(!configFile.empty())
-  {
-    loadConfig(configFile, settings);
   }
 }
 
+using SettingHandler = std::function<void(std::string const&,Settings&)>;
+using SettingHandlerMap = std::unordered_map<std::string, SettingHandler>;
+SettingHandlerMap const SETTING_HANDLERS = {
+  { "mouse.sensitivity", [](std::string const& value, Settings& settings)
+    {
+      settings.mouseSensitivity = std::stoi(value);
+    }
+  },
+  { "mouse.deadzone", [](std::string const& value, Settings& settings)
+    {
+      settings.mouseDeadzone = std::stoi(value);
+    }
+  },
+  { "mouse.wheel.deadzone", [](std::string const& value, Settings& settings)
+    {
+      settings.mouseWheelDeadzone = std::stoi(value);
+    }
+  },
+  { "nubs.left.x", [](std::string const& value, Settings& settings)
+    {
+      settings.leftNubModeX = parseNubAxisMode(value);
+    }
+  },
+  { "nubs.left.y", [](std::string const& value, Settings& settings)
+    {
+      settings.leftNubModeY = parseNubAxisMode(value);
+    }
+  },
+  { "nubs.right.x", [](std::string const& value, Settings& settings)
+    {
+      settings.rightNubModeX = parseNubAxisMode(value);
+    }
+  },
+  { "nubs.right.y", [](std::string const& value, Settings& settings)
+    {
+      settings.rightNubModeY = parseNubAxisMode(value);
+    }
+  },
+  { "nubs.left.click", [](std::string const& value, Settings& settings)
+    {
+      settings.leftNubClickMode = parseNubClickMode(value);
+    }
+  },
+  { "nubs.right.click", [](std::string const& value, Settings& settings)
+    {
+      settings.rightNubClickMode = parseNubClickMode(value);
+    }
+  }
+};
+
 void loadConfig(std::string const& filename, Settings& settings)
 {
-  std::regex re("^(\\w+)\\s*=\\s*(\\w*)$");
+  std::regex re("^([\\w.]+)\\s*=\\s*(.*)$");
   std::regex emptyRe("^\\s*$");
   std::ifstream configFile(filename);
 
@@ -196,24 +274,51 @@ void loadConfig(std::string const& filename, Settings& settings)
       std::string key = match[1];
       std::string value = match[2];
       std::transform(key.begin(), key.end(), key.begin(), tolower);
-
-      if(key == "mousesensitivity")
+      auto iter = SETTING_HANDLERS.find(key);
+      if(iter == SETTING_HANDLERS.end())
       {
-        settings.mouseSensitivity = std::stoi(value);
+        std::cout << "WARNING: Unknown setting in config file: " 
+          << key << std::endl;
       }
-      else if(key == "mousedeadzone")
+      else
       {
-        settings.mouseDeadzone = std::stoi(value);
-      }
-      else if(key == "mousewheeldeadzone")
-      {
-        settings.mouseWheelDeadzone = std::stoi(value);
+        iter->second(value, settings);
       }
     }
     else
     {
       std::cerr << "Invalid line in config file: " << line << std::endl;
     }
+  }
+}
+
+Settings::NubAxisMode parseNubAxisMode(std::string const& str)
+{
+  std::string s(str);
+  std::transform(s.begin(), s.end(), s.begin(), tolower);
+  auto iter = NUB_AXIS_MODES.find(s);
+  if(iter == NUB_AXIS_MODES.end())
+  {
+    return Settings::UNKNOWN_NUB_AXIS_MODE;
+  }
+  else
+  {
+    return iter->second;
+  }
+}
+
+Settings::NubClickMode parseNubClickMode(std::string const& str)
+{
+  std::string s(str);
+  std::transform(s.begin(), s.end(), s.begin(), tolower);
+  auto iter = NUB_CLICK_MODES.find(s);
+  if(iter == NUB_CLICK_MODES.end())
+  {
+    return Settings::UNKNOWN_NUB_CLICK_MODE;
+  }
+  else
+  {
+    return iter->second;
   }
 }
 
@@ -261,6 +366,8 @@ void handleNubAxis(Settings::NubAxisMode mode, int value, Mouse* mouse, UinputDe
       gamepad->send(EV_ABS, ABS_RY, value);
       gamepad->send(EV_SYN, 0, 0);
       break;
+    case Settings::UNKNOWN_NUB_AXIS_MODE:
+      break;
   }
 }
 
@@ -290,6 +397,8 @@ void handleNubClick(Settings::NubClickMode mode, int value, Mouse* mouse, Uinput
       gamepad->send(EV_KEY, BTN_THUMBR, value);
       gamepad->send(EV_SYN, 0, 0);
       break;
+  case Settings::UNKNOWN_NUB_CLICK_MODE:
+      break;
   }
 }
 
@@ -307,20 +416,51 @@ void handleMouse(Mouse* mouse, Settings* settings, bool* stop)
         || mouse->dwy < -settings->mouseWheelDeadzone)
     {
       std::lock_guard<std::mutex> lk(mouse->mutex);
-      if(mouse->dx > settings->mouseDeadzone
-          || mouse->dx < -settings->mouseDeadzone)
+
+      if(mouse->dx > settings->mouseDeadzone)
+      {
         mouse->device.send(EV_REL, REL_X, 
-            mouse->dx * settings->mouseSensitivity / 1000);
-      if(mouse->dy > settings->mouseDeadzone
-          || mouse->dy < -settings->mouseDeadzone)
-        mouse->device.send(EV_REL, REL_Y,
-            mouse->dy * settings->mouseSensitivity / 1000);
-      if(mouse->dwx > settings->mouseWheelDeadzone
-          || mouse->dwx < -settings->mouseWheelDeadzone)
-        mouse->device.send(EV_REL, REL_HWHEEL, mouse->dwx > 0 ? 1 : -1);
-      if(mouse->dwy > settings->mouseWheelDeadzone
-          || mouse->dwy < -settings->mouseWheelDeadzone)
-        mouse->device.send(EV_REL, REL_WHEEL, -mouse->dwy > 0 ? 1 : -1);
+            (mouse->dx - settings->mouseDeadzone)
+            * settings->mouseSensitivity / 1000);
+      }
+      else if(mouse->dx < -settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_X, 
+            (mouse->dx + settings->mouseDeadzone)
+            * settings->mouseSensitivity / 1000);
+      }
+
+      if(mouse->dy > settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_Y, 
+            (mouse->dy - settings->mouseDeadzone)
+            * settings->mouseSensitivity / 1000);
+      }
+      else if(mouse->dy < -settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_Y, 
+            (mouse->dy + settings->mouseDeadzone)
+            * settings->mouseSensitivity / 1000);
+      }
+
+      if(mouse->dwx > settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_HWHEEL, 1);
+      }
+      else if(mouse->dwx < -settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_HWHEEL, -1);
+      }
+
+      if(mouse->dwy > settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_WHEEL, -1);
+      }
+      else if(mouse->dwy < -settings->mouseDeadzone)
+      {
+        mouse->device.send(EV_REL, REL_WHEEL, 1);
+      }
+
       mouse->device.send(EV_SYN, 0, 0);
     }
     else
@@ -332,3 +472,4 @@ void handleMouse(Mouse* mouse, Settings* settings, bool* stop)
   }
   delete mouse;
 }
+
